d5dfb489c8aef10116a6a968f8a0923408e36a61
Treeeest <tristanheilman@outlook.com>
2020-10-07 16:49:28 -0400

    updates to express api

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const mysql = require('mysql');
 const { Connection, Request } = require("tedious");
 
 const app = express();
-
-const port = process.env.PORT || 80
+const port = 8000;
 
 // Create connection to database
 const config = {
@@ -22,51 +21,60 @@ const config = {
     encrypt: true
   }
 };
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
 // API Functions
 app.get('/api/users', (req, res) => {
-  const connection = new Connection(config);
-  console.log("CONNECTION: ", connection);
-  
-  // Attempt to connect and execute queries if connection goes through
-  connection.on("connect", err => {
-    if (err) {
-      console.error(err.message);
-    } else {
-        console.log("HERE")
-        queryDatabase(connection, res);
-    }
-  });
+    const connection = new Connection(config);
+    console.log("CONNECTION: ", connection);
+
+    // Attempt to connect and execute queries if connection goes through
+    connection.on("connect", err => {
+      if (err) {
+        console.error(err.message);
+      } else {
+          console.log("HERE")
+          queryDatabase(connection, res);
+      }
+    });
 });
 
 // Query Functions
 function queryDatabase(conn, result) {
-  console.log("Reading rows from the Table...");
+    console.log("Reading rows from the Table...");
 
-  // Read all rows from table
-  let data = [];
-  const request = new Request(
-      `SELECT * FROM [dbo].[Teams]`,
-      (err, rowCount, rows) => {
-          if (err) {
-              result.send({ status: 500, data: null, message: "internal server error."});
-          } else {
-              result.send({ status: 200, data: data, message: "OK"});
-          }
-      }
-  );
+    // Read all rows from table
+    let data = [];
+    const request = new Request(
+        `SELECT * FROM [dbo].[Teams]`,
+        (err, rowCount, rows) => {
+            if (err) {
+                result.send({ status: 500, data: null, message: "internal server error."});
+            } else {
+                result.send({ status: 200, data: data, message: "OK"});
+            }
+        }
+    );
 
-  request.on('row', function(row){
-      data.push({
-          id: row[0].value,
-          team: row[1].value,
-          LastYearPoints: row[2].value,
-          ThisYearPoints: row[3].value
-      });
-  })
+    request.on('row', function(row){
+        data.push({
+            id: row[0].value,
+            team: row[1].value,
+            LastYearPoints: row[2].value,
+            ThisYearPoints: row[3].value
+        });
+    })
 
-  conn.execSql(request);
+    conn.execSql(request);
 }
+
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`App server now listening to port ${port}`);
